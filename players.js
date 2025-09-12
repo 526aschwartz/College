2b762976486467b9032817aca38cd058a928ffd7
526aschwartz <526aschwartz@frhsd.com>
2025-09-12 11:53:49 -0400

    added major page

---
+++ players.js
@@ -0,0 +1,92 @@
+const players = [
+    {
+        firstName: "Agricultural Sciences",
+        age: "Develop impactful solutions while discovering why the College of Agricultural Sciences is a world leader in agricultural research and learning.",
+        photo: 'imgs/agricultural sciences.webp',
+        link: 'https://agsci.psu.edu/'
+    },
+    {
+        firstName: "Arts and Architecture",
+        age: "Immerse yourself in a vibrant arts and design community including academic programs in music, theatre, visual arts, graphic design, art history, architecture, and landscape architecture.",
+        photo: 'imgs/arts and acrheticutre.webp',
+        link: 'https://arts.psu.edu/'
+    },
+    {
+        firstName: "Smeal College of Business",
+        age: "Become part of a vibrant intellectual community offering highly ranked undergraduate, graduate, doctoral, and executive business education opportunities.",
+        photo: 'imgs/smeal college f business.webp',
+        link: 'https://smeal.psu.edu/'
+    },
+    {
+        firstName: "Donald P. Bellisario College of Communications",
+        age: "Join the world of news, advertising, or public relations, where programs mix practical experience and academic rigor.",
+        photo: 'imgs/Donald P. Bellisario College of Communications.webp',
+        link: 'https://www.bellisario.psu.edu/'
+    },
+    {
+        firstName: "Earth and Mineral Sciences",
+        age: "Impact the planet’s future through programs in natural sciences, social science, and engineering offered by the College of Earth and Mineral Sciences.",
+        photo: 'imgs/earyth and mineral sciences.webp',
+        link: 'https://www.ems.psu.edu/'
+    },
+    {
+        firstName: "Education",
+        age: "Learn to teach from more than twenty teacher preparation programs, including a Professional Development School that has won four major national awards.",
+        photo: 'imgs/esucation.webp',
+        link: 'https://ed.psu.edu/'
+    },
+    {
+        firstName: "Engineering",
+        age: "Become a world-class engineer who learns to use fundamentals and broad-based thinking to discover innovative solutions for worldwide issues.",
+        photo: 'imgs/engineering-college.webp',
+        link: 'https://www.engr.psu.edu/'
+    },
+    {
+        firstName: "Health and Human Development",
+        age: "Become a world-class engineer who learns to use fundamentals and broad-based thinking to discover innovative solutions for worldwide issues.",
+        photo: 'imgs/health and human development.webp',
+        link: 'https://hhd.psu.edu/'
+    },
+     {
+        firstName: "Information Sciences and Technology",
+        age: "Solve real-world problems at the intersection of information, technology, and society through collaborative and interdisciplinary learning opportunities.",
+        photo: 'imgs/information sciences and technology.webp',
+        link: 'https://ist.psu.edu/'
+    },
+     {
+        firstName: "School of International Affairs",
+        age: "Prepare for your career as a global leader in a complex world with a professional and interdisciplinary Master of International Affairs degree.",
+        photo: 'imgs/School of International Affairs.webp',
+        link: 'https://www.sia.psu.edu/'
+    },
+    {
+        firstName: "Penn State Dickinson Law",
+        age: "Penn State Dickinson Law, Pennsylvania’s oldest public law school, offers a reimagined legal education at two central Pennsylvania locations.",
+        photo: 'imgs/Penn State Dickinson Law.webp',
+        link: 'https://dickinsonlaw.psu.edu/'
+    },
+     {
+        firstName: "The Liberal Arts",
+        age: "Learn to recognize problems, create solutions, and combine your liberal arts education and enriching out-of-class experiences to make an impact.",
+        photo: 'imgs/The Liberal Arts.webp',
+        link: 'https://la.psu.edu/'
+    },
+     {
+        firstName: "Medicine",
+        age: "From patient health care services to research, learn the art and science of medicine with the College of Medicine’s world-class faculty and programs.",
+        photo: 'imgs/Medicine.webp',
+        link: 'https://med.psu.edu/'
+    },
+     {
+        firstName: "Nese College of Nursing",
+        age: "Join the next generation of nurses and nurse scientists while enhancing the health of all people through quality care and education..",
+        photo: 'imgs/Nese College of Nursing.webp',
+        link: 'https://www.nursing.psu.edu/'
+    },
+     {
+        firstName: "Eberly College of Science",
+        age: "Become a scientifically trained leader and innovator prepared to enrich lives, improve society, and address global challenges.",
+        photo: 'imgs/Eberly College of Science.webp',
+        link: 'https://science.psu.edu/'
+    },
+]
